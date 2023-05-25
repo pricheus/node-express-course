3e780b53b9cf31bb8f7865f0c51db79a665bc79f
Elie <precieuxbatonda@gmail.com>
2023-05-25 21:53:12 +0200

    just our first middleware

---
+++ 02-express-tutorial/middleware.js
@@ -0,0 +1,10 @@
+let  middleware = function(req, res, next){
+    const method = req.method;
+          const url = req.url;
+          const date = new Date().getFullYear();
+          console.log(method, url, date);
+          next();
+  };
+
+
+ module.exports = middleware;
