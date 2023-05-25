5065b7a1fc52eee4fe13e9bbfd6e899497234dad
Elie <precieuxbatonda@gmail.com>
2023-05-25 21:54:21 +0200

    mess

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,35 +1,29 @@
 const express = require('express');
 const app = express();
-const {products} = require('./data')
+const middleware = require('./middleware');
+const authorize = require('./middleware 2');
 
-app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
-})
+    // req => middlewaree => res
+    app.use([middleware, authorize])
 
-    //return the data in a more inbound way
-app.get('/api/products/', (req, res) => {
-    const newProduct = products.map(function (prod){
-        const {id, name, image} = prod;
-         return {id, name, image}
-        });
-        res.json(newProduct)
-});
-    //return the first element of the array
-    app.get('/api/products/:productID', (req, res) => {
+    app.get('/', (req,res) => {
+      
+        res.send('<h1>Home page</h1>');
+        
+    });
+    app.get('/api/products', (req,res) => {
+        res.send('<h1>product</h1>')
+    });
+    app.get('/about', (req,res) => {
+        res.send('<h1>About page</h1>')
+    });
+    app.get('/api/items', (req,res) => {
+        res.send('<h1>Item</h1>')
+    });
 
-    const {productID} = req.params;
-            //if someone asks a product that does not exist
-    const singleProd = products.find(function(prod){
-        return prod.id === Number(productID)
-    })
 
-    if(!singleProd){
-        return res.status(404).send('<h1>Product does not exist</h1>')
-    }
 
-    res.json(singleProd)
-})
 
-app.listen(5000, () => {
-    console.log('Server listens to port 5000...')
+app.listen(5000,() => {
+    console.log('server listens to port 5000...')
 });
